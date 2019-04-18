7f1fa73ffa2a268e81eea0b31c6ee39f4a411a91
Federico Mulas <mulas.federico.w@gmail.com>
2019-04-18 17:41:39 +0200

    Starting of kademlia project. Fresh inizialized maven project

---
+++ src/main/java/kademlia/Coordinator.java
@@ -0,0 +1,13 @@
+package kademlia;
+
+/**
+ * Hello world!
+ *
+ */
+public class Coordinator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
